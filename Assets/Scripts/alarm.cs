b8e040b2310fe3a02a28ac859cc15b8ed104c2b0
SaulBadmannn <66533036+SaulBadmannn@users.noreply.github.com>
2021-02-24 00:15:14 +0500

    init

---
+++ Assets/Scripts/alarm.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alarm : MonoBehaviour
+{
+    [SerializeField] public AudioSource alarmSound;
+    private bool isActivated = false;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.name == "rogue")
+        {
+            if(isActivated == false)
+            {
+                Debug.Log("Запустить сигналку!");
+                alarmSound.Play();
+                isActivated = true;
+                var alarmIsPlay = StartCoroutine(isYowl());
+
+            }
+            else
+            {
+                Debug.Log("Выключить сигналку!");
+                isActivated = false;
+                alarmSound.Stop();
+                StopCoroutine(isYowl());
+            }
+        }
+    }
+
+    private IEnumerator isYowl()
+    {
+        while (isActivated == true)
+        {
+            while (alarmSound.volume > 0)
+            {
+                alarmSound.volume -= 0.1f;
+                yield return new WaitForSeconds(0.3f);
+            }
+            while (alarmSound.volume < 1)
+            {
+                alarmSound.volume += 0.1f;
+                yield return new WaitForSeconds(0.3f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/rogueMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rogueMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 0;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(horizontalInput * _speed * Time.deltaTime, verticalInput * _speed * Time.deltaTime, 0);
+
+    }
+}
